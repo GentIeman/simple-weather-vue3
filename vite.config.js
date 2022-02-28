a5e3daf0c6c2692d7d753f85ab77ffe832edbcd7
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-28 03:25:01 +0300

    feat: import global styles

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
-import { fileURLToPath, URL } from "url";
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-// eslint-disable-next-line no-undef
-import path from "path";
+import { fileURLToPath, URL } from "url"
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
   },
   css: {
     preprocessorOptions: {
-      stylus: {
-        imports: [path.resolve(__dirname, "src/assets/stylus.styl")],
-      },
-    },
-  },
-});
+      sass: {
+        additionalData: `@import './src/assets/styles/base.sass'`
+      }
+    }
+  }
+})
