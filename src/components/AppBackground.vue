b1d6f703da547be6140164dcfba15e8e0df6350c
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-28 03:23:52 +0300

    feat: background component created

---
+++ src/components/AppBackground.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="background-blur"></section>
+</template>
+
+<script>
+export default {
+  name: "AppBackground"
+}
+</script>
+
+<style scoped lang="stylus">
+.background-blur
+  position fixed
+  top 0
+  left 0
+  width 100%
+  height 100%
+  background no-repeat center / 90% url("@/static/icons/background.svg")
+  z-index -1
+</style>
