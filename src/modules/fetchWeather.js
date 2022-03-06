e2de83edae943ded3d261e44b6421725183fed02
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-03-06 15:57:06 +0300

    update: using an environment variable

--- src/modules/fetchWeather.js
+++ src/modules/fetchWeather.js
@@ -1,9 +1,10 @@
 import { referenceError } from "./referenceError"
-import conf from "@/conf.json"
 
 export const fetchWeather = async (city) => {
   try {
-    let res = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${conf.key}&q=${city}&days=3`)
+    let res = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=${city}&days=3`
+    )
       .then((data) => data.json())
       .catch((err) => console.error(err))
 
