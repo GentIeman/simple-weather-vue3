5af86a438f8bf97b1888f83268410c34ce45eb06
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-03-07 20:08:59 +0300

    update: rules for formatting Vue code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,10 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "env": {
+  root: true,
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  env: {
     "vue/setup-compiler-macros": true
   }
 }
