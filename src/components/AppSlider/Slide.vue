01f835df26216b368d061855635f22e41ddbc038
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-03-15 22:53:55 +0300

    feat: slider created

---
+++ src/components/AppSlider/Slide.vue
@@ -0,0 +1,38 @@
+<template>
+  <li class="slide">
+    <header class="slide__header">
+      <h2 class="slide__day"></h2>
+      <time class="slide__date">{{ gettingDate(day.date) }}</time>
+    </header>
+    <div class="slide__weather">
+      <img :src="day.day.condition.icon" alt="" width="64" height="64" />
+      <p class="slide__temp">{{ roundingWeatherParams(day.day.maxtemp_c) }}°</p>
+    </div>
+    <footer class="slide__footer">
+      <p class="slide__condition">{{ day.day.condition.text }}</p>
+      <p class="slide__night">
+        At night: {{ roundingWeatherParams(day.day.mintemp_c) }}°
+      </p>
+    </footer>
+  </li>
+</template>
+
+<script setup>
+import { roundingWeatherParams } from "@/modules/roundingWeatherParams.js"
+
+const props = defineProps({
+  day: {
+    type: Object,
+    required: true
+  }
+})
+
+const gettingDate = (item) => {
+  return new Date(item).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric"
+  })
+}
+</script>
+
+<style scoped lang="sass"></style>
---
+++ src/components/AppSlider/Slider.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="slider">
+    <ul class="slider__list">
+      <Slide v-for="day in forecastDays" :key="day.id" :day="day" />
+    </ul>
+  </section>
+</template>
+
+<script setup>
+import Slide from "@/components/AppSlider/Slide.vue"
+import { forecastDays } from "@/modules/fetchWeather.js"
+</script>
+
+<style scoped lang="sass">
+.slider
+  margin-top: 30px
+  width: 100%
+  height: auto
+
+  &__list
+    display: flex
+    justify-content: space-around
+    list-style: none
+
+  &__card
+    width: 231px
+    height: 250px
+    background-color: #fff
+    border-radius: 10px
+</style>
