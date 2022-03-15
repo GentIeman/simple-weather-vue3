11a3cd5642c43fafdba2c745d42bfe97f10640cc
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-03-16 00:29:36 +0300

    update: css styles

--- src/components/AppSlider/Slide.vue
+++ src/components/AppSlider/Slide.vue
@@ -35,4 +35,35 @@ const gettingDate = (item) => {
 }
 </script>
 
-<style scoped lang="sass"></style>
+<style scoped lang="sass">
+.slide
+  display: grid
+  width: 200px
+  min-height: 240px
+  padding: 20px
+  background-color: #fff
+  border-radius: 10px
+  word-break: break-word
+  gap: 10px 0
+
+  &__day
+    @include font-style(bold, 1.3rem, "Sergio UI", $accent-color)
+
+  &__date
+    @include font-style(normal, 1rem, "Sergio UI", #AEAEAE)
+
+  &__weather
+    display: flex
+    justify-content: center
+    align-items: center
+
+  &__temp
+    @include font-style(bold, 2.4rem, "Sergio UI", $primary-color)
+
+  &__condition
+    @include font-style(normal, 1.2rem, "Sergio UI", $primary-color)
+
+  &__night
+    padding-top: 5px
+    @include font-style(normal, 1rem, "Sergio UI", #AEAEAE)
+</style>
