11f35234373b6135abf12895df860a856e0aab3e
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-03-26 03:30:59 +0300

    fix: changed style lang

--- src/components/AppBackground.vue
+++ src/components/AppBackground.vue
@@ -8,13 +8,13 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus">
+<style scoped lang="sass">
 .background-blur
-  position fixed
-  top 0
-  left 0
-  width 100%
-  height 100%
-  background no-repeat center / 90% url("@/static/icons/background.svg")
-  z-index -1
+  position: fixed
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+  background: no-repeat center / 90% url("@/static/icons/background.svg")
+  z-index: -1
 </style>
